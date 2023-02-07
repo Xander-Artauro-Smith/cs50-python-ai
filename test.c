9242ca8e5020adfcb727c1d818f46ffd89d14fd5
Xander-Artauro-Smith <122746075+Xander-Artauro-Smith@users.noreply.github.com>
2023-02-07 16:29:09 -0500

    Create test.c

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void){
+
+    //string answer = get_string("type boulder here: ");
+    string strizzy = "bald";
+    printf("hello world\n%s", strizzy);
+    //printf("yes %s", answer);
+    
+}
